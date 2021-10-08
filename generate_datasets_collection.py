c0ecd726d4880532e84a209acc2d2e473c9074b5
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2021-10-07 21:16:57 -0400

    Add code to generate Anserini-friendly dataset search collection

--- generate_datasets_collection.py
+++ generate_datasets_collection.py
@@ -1,3 +1,12 @@
+'''
+python generate_datasets_collection.py
+
+This script converts the full set of datasets from PapersWithCode into a jsonlines file, read to be consumed into an
+Anserini search index.
+
+Each row must have at least two columns: "id" (dataset name) and "contents" (description), among other fields.
+'''
+
 import argparse
 import glob
 import gzip
@@ -21,21 +30,46 @@ def load_metadata_iterator(metadata_files):
         for row in jsonlines.open(f):
             yield row
 
-def construct_dataset_description_mapping(pwc_datasets_file):
-    with open(pwc_datasets_file, 'rb') as f:
-        pwc_datasets_metadata = json.load(f)
-        dataset_description_mapping = {}
-        for dataset in pwc_datasets_metadata:
-            dataset_description_mapping[dataset["name"]] = dataset.get("description", "")
-        return dataset_description_mapping
+def construct_dataset_to_description_mapping(pwc_datasets_metadata):
+    dataset_description_mapping = {}
+    for dataset in pwc_datasets_metadata:
+        dataset_description_mapping[dataset["name"]] = dataset.get("description", "")
+    return dataset_description_mapping
 
-def construct_dataset_document_collection(jsonl_writer, dataset_s2orc_id_mapping, dataset_descriptions, s2orc_full_text_directory, s2orc_metadata_directory):
-    dataset_metadata = {}
+def construct_paper_info_to_dataset_mapping(pwc_datasets_metadata):
+    paper_title_to_datasets_map = {}
+    paper_url_to_datasets_map = {}
+    for dataset in pwc_datasets_metadata:
+        paper_meta = dataset.get("paper")
+        if paper_meta is None:
+            paper_meta = {}
+        dataset_paper_title = paper_meta.get("title")
+        dataset_url = paper_meta.get("url")
+        if dataset_paper_title is not None:
+            paper_title_to_datasets_map[dataset_paper_title] = dataset["name"]
+        if dataset_url is not None:
+            paper_url_to_datasets_map[dataset_url] = dataset["name"]
+    return paper_title_to_datasets_map, paper_url_to_datasets_map
+
+def construct_dataset_to_variants_mapping(pwc_datasets_metadata):
+    dataset_variants_mapping = {}
+    for dataset in pwc_datasets_metadata:
+        dataset_variants_mapping[dataset["name"]] = dataset.get("variants", "")
+    return dataset_variants_mapping
+
+def construct_dataset_document_collection(jsonl_writer, dataset_s2orc_id_mapping, pwc_datasets_metadata, s2orc_full_text_directory, s2orc_metadata_directory):
+    dataset_to_descriptions = construct_dataset_to_description_mapping(pwc_datasets_metadata)
+    dataset_to_variants = construct_dataset_to_variants_mapping(pwc_datasets_metadata)
+    paper_title_to_datasets_map, paper_url_to_datasets_map = construct_paper_info_to_dataset_mapping(pwc_datasets_metadata)
+    dataset_to_paper_title_map = {dataset:title for title, dataset in paper_title_to_datasets_map.items()}
+
+    s2orc_id_to_dataset_mapping = {}
+    s2orc_ids_search_list = []
     for dataset_name, [s2orc_id] in dataset_s2orc_id_mapping.items():
-        dataset_metadata[s2orc_id] = dataset_name
-    document_search_list = set(dataset_metadata.keys())
+        s2orc_id_to_dataset_mapping[s2orc_id] = dataset_name
+        s2orc_ids_search_list.append(s2orc_id)
 
-    num_documents_written = 0
+    matched_datasets = set()
     full_texts_files = glob.glob(os.path.join(s2orc_full_text_directory, "*.jsonl.gz"))
     for full_text_shard_path in full_texts_files:
         shard_id = full_text_shard_path.split(".jsonl.gz")[0].split("/")[-1]
@@ -50,32 +84,66 @@ def construct_dataset_document_collection(jsonl_writer, dataset_s2orc_id_mapping
         full_text_shard = gzip.open(full_text_shard_path, 'rt')
         s2orc_full_text = jsonlines.Reader(full_text_shard)
         for doc in tqdm(s2orc_full_text):
-            if doc['paper_id'] not in document_search_list:
-                continue
+            dataset_name = None
+            paper_id_match = False
+            paper_title_match = False
             paper_id = doc['paper_id']
 
+            if paper_id in s2orc_ids_search_list:
+                paper_id_match = True
+                dataset_name = s2orc_id_to_dataset_mapping.get(paper_id)
+                s2orc_ids_search_list.remove(paper_id)
+
+            paper_title = metadata[paper_id].get("title")
+            if paper_title in paper_title_to_datasets_map:
+                paper_title_match = True
+                if dataset_name is None:
+                    dataset_name = paper_title_to_datasets_map.get(paper_title)
+                del paper_title_to_datasets_map[paper_title]
+
+            if not (paper_id_match or paper_title_match):
+                continue
+            assert dataset_name is not None
+
             dataset_document = {}
-            dataset_name = dataset_metadata[paper_id]
-            dataset_document["dataset_name"] = dataset_name
-            dataset_document["description"] = dataset_descriptions[dataset_name]
+            dataset_document["id"] = dataset_name
+            dataset_document["contents"] = dataset_to_descriptions[dataset_name]
+            dataset_document["variants"] = dataset_to_variants[dataset_name]
             dataset_document["title"] = metadata[paper_id]["title"]
             dataset_document["abstract"] = metadata[paper_id]["abstract"]
             section_texts = [section["text"] for section in doc["body_text"]]
             body_text = "\n".join(section_texts)
             dataset_document["body_text"] = body_text
+            matched_datasets.add(dataset_name)
             jsonl_writer.write(dataset_document)
-            num_documents_written += 1
 
-            document_search_list.remove(doc['paper_id'])
-            if len(document_search_list) == 0:
-                return
-        print(f"{num_documents_written} documents written to file so far.")
+        print(f"{len(matched_datasets)} documents written to file so far.")
+
+    num_matched_datasets = len(matched_datasets)
+    # Write unmatched datasets
+    for dataset_name in dataset_to_descriptions:
+        if dataset_name not in matched_datasets:
+            dataset_document = {}
+            dataset_document["id"] = dataset_name
+            dataset_title = dataset_to_paper_title_map.get(dataset_name)
+            if dataset_title is not None:
+                dataset_document["title"] = dataset_title
+            dataset_document["contents"] = dataset_to_descriptions[dataset_name]
+            if dataset_name in dataset_to_variants:
+                dataset_document["variants"] = dataset_to_variants[dataset_name]
+            matched_datasets.add(dataset_name)
+            jsonl_writer.write(dataset_document)
+    print(f"Wrote {len(matched_datasets) - num_matched_datasets} datasets that could not be matched to S2orc.")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
     dataset_s2orc_id_mapping = json.load(open(args.dataset_to_s2orc_mapping_file))
-    dataset_descriptions = construct_dataset_description_mapping(args.pwc_datasets_file)
+    with open(args.pwc_datasets_file, 'rb') as f:
+        pwc_datasets_metadata = json.load(f)
+    dataset_descriptions = construct_dataset_to_description_mapping(pwc_datasets_metadata)
+    dataset_variants = construct_dataset_to_variants_mapping(pwc_datasets_metadata)
+
     with open(args.output_file, 'wb') as f:
         writer = jsonlines.Writer(f)
-        construct_dataset_document_collection(writer, dataset_s2orc_id_mapping, dataset_descriptions, args.s2orc_full_text_directory, args.s2orc_metadata_directory)
+        construct_dataset_document_collection(writer, dataset_s2orc_id_mapping, pwc_datasets_metadata, args.s2orc_full_text_directory, args.s2orc_metadata_directory)
