36212e879b0eb7240edfdf52fc2e3cb46ceb739a
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2021-10-12 22:44:11 -0400

    Add demo scripts

---
+++ search_demo.sh
@@ -0,0 +1,8 @@
+export PROJ=/projects/ogma1/vijayv/
+export JAVA_HOME=$PROJ/jdk-11
+export PATH=$JAVA_HOME/bin:$PATH
+alias java=$JAVA_HOME/bin/java
+
+source /projects/ogma1/vijayv/anaconda3/etc/profile.d/conda.sh 
+conda activate scirex
+python search_demo.py --anserini-index indexes/dataset_search_collection_no_abstracts_or_paper_text_jsonl
