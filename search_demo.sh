a1a2d9fa739fdf9c9bcb6bd8caa4198bafe00e36
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2021-10-22 13:37:49 -0400

    Add search demo script

--- search_demo.sh
+++ search_demo.sh
@@ -5,4 +5,4 @@ alias java=$JAVA_HOME/bin/java
 
 source /projects/ogma1/vijayv/anaconda3/etc/profile.d/conda.sh 
 conda activate scirex
-python search_demo.py --anserini-index indexes/dataset_search_collection_no_abstracts_or_paper_text_jsonl
+python search_demo_bm25.py --anserini-index indexes/dataset_search_collection_no_abstracts_or_paper_text_jsonl
